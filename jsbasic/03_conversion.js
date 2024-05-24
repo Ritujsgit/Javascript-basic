008c3447bce2361ec0e834f3e532ad581003c0b9
Ritu <141140993+Ritujsgit@users.noreply.github.com>
2024-05-24 11:35:16 +0000

    operators

--- jsbasic/03_conversion.js
+++ jsbasic/03_conversion.js
@@ -1,29 +1,52 @@
-let score = 33
-let score_string = String(score)
-console.log(score_string, typeof score_string); // output will be 33 string
+// let score = 33
+// let score_string = String(score)
+// console.log(score_string, typeof score_string); // output will be 33 string
 
-let score_str = '33'
-let score_Num = Number(score_str)
-console.log(score_Num, typeof score_Num);// output will be 33 number
+// let score_str = '33'
+// let score_Num = Number(score_str)
+// console.log(score_Num, typeof score_Num);// output will be 33 number
 
-let score_NaN = '33abc'
-let score_Nan2 = Number(score_NaN)
-console.log(score_Nan2, typeof score_Nan2);// output will be NaN number 
+// let score_NaN = '33abc'
+// let score_Nan2 = Number(score_NaN)
+// console.log(score_Nan2, typeof score_Nan2);// output will be NaN number 
 
-//--------------------------------TRUE FALSE---------------------------------
+// //--------------------------------TRUE FALSE---------------------------------
 
-let isLoggedIn = 1
-let isLoggedIn_Bool = Boolean(isLoggedIn)
-console.log(isLoggedIn_Bool, typeof isLoggedIn_Bool);// output will be True Boolean 
+// let isLoggedIn = 1
+// let isLoggedIn_Bool = Boolean(isLoggedIn)
+// console.log(isLoggedIn_Bool, typeof isLoggedIn_Bool);// output will be True Boolean 
 
-let isNotLoggedIn = 0
-let isNotLoggedIn_Bool = Boolean(isNotLoggedIn)
-console.log(isNotLoggedIn_Bool, typeof isNotLoggedIn_Bool);// output will be False Boolean
+// let isNotLoggedIn = 0
+// let isNotLoggedIn_Bool = Boolean(isNotLoggedIn)
+// console.log(isNotLoggedIn_Bool, typeof isNotLoggedIn_Bool);// output will be False Boolean
 
-let isNotLoggedIn_str = ""
-let isNotLoggedIn_Bool_str = Boolean(isNotLoggedIn_str)
-console.log(isNotLoggedIn_Bool_str, typeof isNotLoggedIn_Bool_str);// output will be False Boolean
+// let isNotLoggedIn_str = ""
+// let isNotLoggedIn_Bool_str = Boolean(isNotLoggedIn_str)
+// console.log(isNotLoggedIn_Bool_str, typeof isNotLoggedIn_Bool_str);// output will be False Boolean
 
-let isLoggedIn_str = "Hello"
-let isLoggedIn_str_Bool = Boolean(isLoggedIn_str)
-console.log(isLoggedIn_str_Bool, typeof isLoggedIn_str_Bool);// output will be True Boolean 
+// let isLoggedIn_str = "Hello"
+// let isLoggedIn_str_Bool = Boolean(isLoggedIn_str)
+// console.log(isLoggedIn_str_Bool, typeof isLoggedIn_str_Bool);// output will be True Boolean 
+
+//-------------------Operators--------------------
+// +, -,*, /, %, **
+//------------------------------------------------
+
+//-----------Concatinate 2 strings
+str1 ="Hello "
+str2 = "Ritu"
+console.log(str1 + str2); // output: Hello Ritu
+
+console.log(1 + 2); //output: 3
+console.log("1" + 2); //output: 12 
+console.log(1 + "2"); //output: 12
+// + operator works to add two number or two string.
+// if it gets 1st number and second string then both will be treaded as string bcoz the first number is string
+// but if it gets 1st and 2nd number and 3rd string then it will add first two number and then concat with the third string
+console.log(1 + 3+ "2"); //output: 42
+console.log(1 + 3 + 5 + "2"); //output: 92
+
+
+
+
+
